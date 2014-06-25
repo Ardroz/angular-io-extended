f66bd22caf2862740e3f93795fa0a4ceb20ea35e
Ardroz <darr.0725@gmail.com>
2014-06-25 12:30:43 -0500

    Step 3

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 var app = angular.module( 'myApp', [] );
 
 app.run( init );
+app.controller( 'Hola', hola );
+app.controller( 'Adios', adios );
 
 init.$inject = ['$rootScope'];
 function init ( $rootScope ) {
   console.log( $rootScope );
   $rootScope.nombre = 'Daniel';
+}
+
+hola.$inject = ['$scope'];
+function hola ( $scope ) {
+  console.log('Hola : ');
+  console.log( $scope );
+}
+
+adios.$inject = ['$scope'];
+function adios ( $scope ) {
+  console.log('Adios : ');
+  console.log( $scope );
 }
